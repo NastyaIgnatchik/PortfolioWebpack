9fa39c072cf6e88f82d21a2ba8ef61c2e111a776
AzazelLittle <nastechka.ignatchik@gmail.com>
2023-06-18 01:11:04 +0300

    Feature: adaptive services page.

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { BsCheck2 } from "react-icons/bs";
+
+import style from "./servicesStyle.module.less";
+
+interface IServices {
+  service: string;
+}
 
 const Services = () => {
-  return <div>services</div>;
+  const arrayForCount = [1, 2, 3, 4, 5, 6];
+
+  const services = [
+    {
+      service: "UI/UX Design",
+    },
+    {
+      service: "Web Development",
+    },
+    {
+      service: "Content Creation",
+    },
+  ];
+
+  return (
+    <div className={style.container}>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className={style.containerForArticles}>
+        {services.map((element: IServices) => {
+          return (
+            <article className={style.article}>
+              <div className={style.articleHead}>
+                <h3>{element.service}</h3>
+              </div>
+              <ul>
+                {arrayForCount.map((num: number) => {
+                  return (
+                    <li key={num}>
+                      <BsCheck2 className={style.items} />
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.{" "}
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
