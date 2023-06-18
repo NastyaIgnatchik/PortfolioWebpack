ca69ce09bf80e80390132b7e66b6da3fb33a4ee3
AzazelLittle <nastechka.ignatchik@gmail.com>
2023-06-19 00:09:21 +0300

    Feature: adaptive portfolio page.

---
+++ data/portfolioData.ts
@@ -0,0 +1,42 @@
+import ntf from "../src/assets/images/netflix.jpeg";
+import prt1 from "../src/assets/images/portfolio1.png";
+import prt3 from "../src/assets/images/portfolio3.jpg";
+import spch from "../src/assets/images/spinach12.jpg";
+import thy from "../src/assets/images/thyliar.png";
+
+export const dataPortfolio = [
+  {
+    id: 1,
+    image: prt1,
+    title: "Crypto Currency Dashboard",
+  },
+  {
+    id: 2,
+    image: ntf,
+    title: "Netflix Clone",
+    github: "https://github.com/NastyaIgnatchik/netflix-clone",
+    demo: "https://netflix-clone-mauve-zeta.vercel.app/login",
+  },
+  {
+    id: 3,
+    image: spch,
+    title: "WorkUp Parser",
+    github: "https://github.com/ErmolaevAlexey365/spinach_learning_frontend",
+  },
+  {
+    id: 4,
+    image: thy,
+    title: "Landing Page",
+    demo: "https://miramurmmurmur.000webhostapp.com/",
+  },
+  {
+    id: 5,
+    image: prt3,
+    title: "Infographics in Figma",
+  },
+  {
+    id: 6,
+    image: prt3,
+    title: "Crypto Currency Dashboard",
+  },
+];
